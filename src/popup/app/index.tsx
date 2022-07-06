c34d512e8fa0a2e8b8ec5739a73b765618b87821
Daniel Lazar <lazardani0112@gmail.com>
2022-07-06 22:56:13 +0200

    feat(popup-app): set min window sizes & add some basic functionalities

--- src/popup/app/index.tsx
+++ src/popup/app/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import Layout from './components/layout';
+import {TablicationData} from '../../shared/types';
 
 export default function App() {
+  useEffect(() => {
+    chrome.runtime.sendMessage({name: 'get-tablication-data'})
+      .then((response: TablicationData) => console.log(response));
+  }, []);
+
   return (
-    <>
-      <header>Popup</header>
-      <main>Content</main>
-      <footer>Copyright</footer>
-    </>
+    <Layout>
+      <p>Hello world!</p>
+    </Layout>
   );
 }
