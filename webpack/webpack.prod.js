be8955861bcc0cf4091a0d625c68e231a31a5d56
Daniel Lazar <lazardani0112@gmail.com>
2022-07-09 15:20:48 +0200

    build(webpack): fix webpack config & add package script for production

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  output: {
+    path: path.join(__dirname, '../build/js'),
+  },
   mode: 'production'
 });
